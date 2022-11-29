38c9fc059c345f70aef1861997ff35a693fcfe7d
DELL <h9553014>
2022-11-29 22:43:12 +0200

    option

--- app/src/main/java/com/example/projectlay/MainActivity.java
+++ app/src/main/java/com/example/projectlay/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.projectlay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button go_to_do;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        go_to_do = findViewById(R.id.go_to_do);
+        go_to_do.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),toDoList.class));
+            }
+        });
     }
 }
